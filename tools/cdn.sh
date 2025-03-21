887337eba81fcc731b5c3fc6521316ad428e5df6
Nikolai Kavtur <nikolai.kavtur@okta.com>
2025-03-21 12:33:00 +0100

    fix: cdn.sh

--- tools/cdn.sh
+++ tools/cdn.sh
@@ -49,11 +49,14 @@ upload_bundle() {
       exit 1
   fi
 
-  if ! file_exists_in_s3 "$S3_PATH" "$bundle"; then
-    upload_to_s3 "$bundle_local_path" "$bundle_s3_path" ""
-  else
-    echo "There is already a $bundle in the cdn. Bundle upload skipped..."
+  if [[ "$MODE" != "dev" ]]; then
+    if file_exists_in_s3 "$S3_PATH" "$bundle"; then
+      echo "There is already a $bundle in the cdn. Bundle upload skipped..."
+      return
+    fi
   fi
+
+  upload_to_s3 "$bundle_local_path" "$bundle_s3_path" ""
 }
 
 upload_assets() {
@@ -64,9 +67,11 @@ upload_assets() {
     "manifest.json"
   )
 
-  if file_exists_in_s3 "$S3_PATH/assets" "${assets[0]}"; then
-    echo "There is already a ${assets[0]} in the cdn. Frontend assets upload skipped..."
-    return
+  if [[ "$MODE" != "dev" ]]; then
+    if file_exists_in_s3 "$S3_PATH/assets" "${assets[0]}"; then
+      echo "There is already a ${assets[0]} in the cdn. Frontend assets upload skipped..."
+      return
+    fi
   fi
 
   for asset in "${assets[@]}"; do
@@ -82,10 +87,11 @@ upload_assets() {
   done
 }
 
+MODE="$1" # dev or prod
 CURRENT_VERSION=$(node tools/get_version.js)
 EXTENSION_NAME="auth0-delegated-admin"
 REGION="us-west-1"
-S3_PATH=$(resolve_s3_path "$1")
+S3_PATH=$(resolve_s3_path "$MODE")
 
 upload_bundle
 upload_assets
